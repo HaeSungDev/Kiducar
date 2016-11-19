0bf1191dbd47d37cc7dda1933747444d4fab7c81
Hae Sung <xods1025@gmail.com>
2016-11-19 16:01:04 +0900

    modify"

--- app/src/main/java/com/kiducar/kiducation/kiducar/BlockCodingActivity.java
+++ app/src/main/java/com/kiducar/kiducation/kiducar/BlockCodingActivity.java
@@ -12,13 +12,5 @@ public class BlockCodingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_block_coding);
-
-        ListView listView = new ListView();
-        ListView.OnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
-            }
-        });
     }
 }
